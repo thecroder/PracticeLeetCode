47c90b8746ebf21443fdfcebb52ae77149b31479
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-26 10:57:08 +0530

    Commit

--- src/com/sanjay/pallindrome.java
+++ src/com/sanjay/pallindrome.java
@@ -1,9 +1,28 @@
 package com.sanjay;
+import java.util.Scanner;
 
 public class pallindrome {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Sanjay");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.next();
+        String org_str=str;
+
+        String rev="";
+        int len = str.length();
+        for (int i =len-1;i>=0;i--){
+            rev = rev+str.charAt(i);
+
+        }
+
+        if(org_str.equals(rev)){
+           System.out.println(org_str + " is Pallindrome");
+        }
+        else{
+            System.out.println(org_str +" is Not Pallindrome");
+        }
+
     }
 }
