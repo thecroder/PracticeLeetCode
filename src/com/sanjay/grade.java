0b96a7f5428b1e4cfb81e4f2807ad4ea19e08e77
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-27 19:36:37 +0530

    Commit

---
+++ src/com/sanjay/grade.java
@@ -0,0 +1,9 @@
+package com.sanjay;
+
+public class grade {
+    public static void main(String[]args) {
+        char grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+    }
+}
