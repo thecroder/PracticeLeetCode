ec7a21615dc356332601737e244227160d4515ff
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-27 20:13:03 +0530

    Commit

---
+++ src/com/sanjay/stringmethod.java
@@ -0,0 +1,11 @@
+package com.sanjay;
+import java.util.Scanner;
+public class stringmethod {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String st = sc.next();
+        //String a  = new String("Sanjay");
+        System.out.println(st);
+    }
+}
