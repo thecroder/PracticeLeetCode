39ea5423636444b6b138bb1d494b7f6d80b47922
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-29 11:07:17 +0530

    Commit

---
+++ src/com/sanjay/conditional.java
@@ -0,0 +1,26 @@
+package com.sanjay;
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // question 1
+//        int a = 11;
+//
+//        if(a==11){
+//            System.out.println("I am 11");
+//        }
+//        else{
+//            System.out.println("I Am not 11");
+//        }
+        // question 2
+        System.out.println("Enter your percentage");
+        int marks = sc.nextInt();
+
+        switch (marks){
+            case 1 : boolean marks>33;
+                System.out.println();
+        }
+
+    }
+}
