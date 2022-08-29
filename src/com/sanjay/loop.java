75f7f294cdf928a386f8cfad1feacf0db0d5297e
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-29 19:23:29 +0530

    Commit

---
+++ src/com/sanjay/loop.java
@@ -0,0 +1,21 @@
+package com.sanjay;
+
+public class loop {
+    public static void main(String[] args){
+        int i = 1;
+        int j = 3;
+        while(i<=1000){
+            System.out.println(i);
+            i++;
+        }
+        while (j<=50){
+            System.out.println(j);
+            j++;
+        }
+//        while(true){
+//            System.out.println("Hello buddy");
+//        }
+
+        //return 0;
+    }
+}
