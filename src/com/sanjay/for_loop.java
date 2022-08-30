6cd0cd6099bd1a4d52c2c5199e0db15afed3df67
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-30 11:18:34 +0530

    Commit

--- src/com/sanjay/for_loop.java
+++ src/com/sanjay/for_loop.java
@@ -2,8 +2,9 @@ package com.sanjay;
 
 public class for_loop {
     public static void main(String[] args){
-        for(int i =5 ; i<=10 ; i++){
-            System.out.println(i);
+        int n =4;
+        for(int i =0 ; i<n ; i++){
+            System.out.println(2*i+1);
         }
     }
 }
