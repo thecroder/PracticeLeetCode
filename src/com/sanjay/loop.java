91bd40d0c0a2e484ecadc5b1d7704a4b626c434d
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-08-30 11:11:46 +0530

    Commit

--- src/com/sanjay/loop.java
+++ src/com/sanjay/loop.java
@@ -2,19 +2,24 @@ package com.sanjay;
 
 public class loop {
     public static void main(String[] args){
-        int i = 1;
-        int j = 3;
-        while(i<=1000){
-            System.out.println(i);
-            i++;
-        }
-        while (j<=50){
-            System.out.println(j);
-            j++;
-        }
+//        int i = 1;
+//        int j = 3;
+//        while(i<=1000){
+//            System.out.println(i);
+//            i++;
+//        }
+//        while (j<=50){
+//            System.out.println(j);
+//            j++;
+//        }
 //        while(true){
 //            System.out.println("Hello buddy");
 //        }
+        int k = 10;
+        do{
+            System.out.println(k);
+            k++;
+        }while(k<5);
 
         //return 0;
     }
