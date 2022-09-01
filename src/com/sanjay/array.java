2df779596b28ba32272e57fcaf38e733571f2ab6
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-09-01 21:39:50 +0530

    Commit

---
+++ src/com/sanjay/array.java
@@ -0,0 +1,14 @@
+package com.sanjay;
+
+public class array {
+    public static void main(String[] args){
+        int[] marks = new int[5];
+        marks[0] = 100;
+        marks[1]=34;
+        marks[2]=334;
+        System.out.println(marks[2]);
+
+        int[] mak = {43,45,3,2,34,55};
+        System.out.println(mak.length);
+    }
+}
