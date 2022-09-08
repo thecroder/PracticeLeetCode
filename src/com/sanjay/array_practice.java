9437f89dfcfee10972906e9ff1d572f5e7ca5c31
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-09-08 10:47:08 +0530

    Leetcode Practice

--- src/com/sanjay/array_practice.java
+++ src/com/sanjay/array_practice.java
@@ -1,4 +1,53 @@
 package com.sanjay;
 
 public class array_practice {
+    public static void main(String[] args) {
+        //Practice problem 1
+        /*float[] marks = {23.3f, 2.3f, 98.4f, 34.3f, 32.4f};
+        float sum = 0;
+        for(float element: marks){
+            sum = sum + element;
+        }
+        System.out.println(sum);
+        //Practice problem 2
+        int [] marks = {23,45,21,43,23,54};
+        int num = 40;
+        boolean isInArray = false;
+        for(int element:marks){
+            isInArray = true;
+            break;
+        }
+        if (isInArray){
+            System.out.println("Present");
+        }
+        else{
+            System.out.println("Not Present");
+        }
+        //Practice problem 3
+        float[] marks = {23.3f, 2.3f, 98.4f, 34.3f, 32.4f};
+        float sum = 0;
+        for(float element: marks){
+            sum = sum + element;
+        }
+        System.out.println(sum/ marks.length);
+        //Practice problem 4
+        int[][] mat1 = {{2, 1, 4}, {3, 2, 6}};
+        int[][] mat2 = {{6, 2, 1}, {5, 7, 3}};
+        int[][] result = {{0, 0, 0}, {0, 0, 0}};
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.format("Setting for value of i=%d, j=%d\n", i, j);
+                result[i][j] = mat1[i][j] + mat2[i][j];
+            }
+            for (i = 0; i < mat1.length; i++) {
+                for (int j = 0; j < mat1[i].length; j++) {
+                    System.out.println(result[i][j] + " ");
+                    result[i][j] = mat1[i][j] + mat2[i][j];
+                }
+                System.out.println("");
+
+            }
+        }*/
+        
+    }
 }
