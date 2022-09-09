d2d5cd7820a1e0016d2e5f56d7e539843599ca8d
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-09-09 07:44:31 +0530

    Leetcode Practice

--- src/com/sanjay/insertion.java
+++ src/com/sanjay/insertion.java
@@ -3,16 +3,18 @@ package com.sanjay;
 import java.util.Scanner;
 
 public class insertion {
-    public static int main(String[] args){
+    public static void main(String[] args){
         int[] arr = {34,43,23,54};
-        System.out.println("Enter the no of elements");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int x = 23;
-        for(int i = 0; i<n;i++){
-            if(arr[i]==x)
-                return i;
+        int x = 40;
+        boolean isInArray = false;
+        for(int element : arr){
+            if(element == x){
+                isInArray = true;
+                System.out.println("Present");
+            }
+            else{
+                System.out.println("Not Present");
+            }
         }
-        return -1;
     }
 }
