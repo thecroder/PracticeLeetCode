982878fbf2543cd19ba3d4db15641fbed96a76c8
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-09-23 03:14:31 +0530

    Leetcode Practice

--- src/com/sanjay/practice.java
+++ src/com/sanjay/practice.java
@@ -2,24 +2,10 @@ package com.sanjay;
 import java.util.Scanner;
 
 public class practice {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
         int n = sc.nextInt();
-        int rev = 0;
-
-        while(n>0){
-           int pop = n%10;
-         rev = rev*10 + pop;
-            n = n/10;
-        }
-//        if(n==rev)
-        System.out.print(rev);
-        {
-            if (rev == n) {
-                System.out.println("Pallindrome");
-            } else {
-                System.out.println("Not Pallindrome");
-            }
-        }
+        int count = 0;
     }
 }
