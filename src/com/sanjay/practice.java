e3919232a12012942cf32ff991004b826afde24c
Sanjay Jaiswal <jaiswalsanjaysj2000@gmail.com>
2022-09-25 15:37:57 +0530

    Leetcode Practice

--- src/com/sanjay/practice.java
+++ src/com/sanjay/practice.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n = sc.nextInt();
-        int count = 0;
+
     }
 }
